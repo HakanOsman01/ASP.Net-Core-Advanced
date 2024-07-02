d5b04bc1248416d16288b2808b69dd1bbd5b7701
Hakan Osman <haki2001@abv.bg>
2024-07-02 11:29:49 +0300

    Finish Skeleton of project

--- ProjectFundamentalsWorkshop/HouseRentingSystem/HouseRentingSystem.Core/Models/Home/IndexViewModel.cs
+++ ProjectFundamentalsWorkshop/HouseRentingSystem/HouseRentingSystem.Core/Models/Home/IndexViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HouseRentingSystem.Core.Models.Home
+﻿namespace HouseRentingSystem.Core.Models.Home
 {
-    internal class IndexViewModel
+    public class IndexViewModel
     {
+
     }
 }
